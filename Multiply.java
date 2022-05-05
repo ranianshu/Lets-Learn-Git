84c606f15b45c2930f4abfe726172ed108808fb2
Anshu Rani <ranianshu484@gmail.com>
2022-05-06 00:25:35 +0530

    added message for multiply

--- Multiply.java
+++ Multiply.java
@@ -3,6 +3,6 @@ public class Multiply {
         int a = 2;
         int b = 3;
 
-        System.out.println("The sum is: "+a*b);
+        System.out.println("The Multiplication is: "+a*b);
     }
 }
